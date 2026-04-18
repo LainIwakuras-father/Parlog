2ec86808768d483f9522de5cecf7a5cc2ba8612d
Astixxx <fake.me100500@gmail.com>
2026-04-18 13:55:46 +0300

    parcer demo impl

--- main.cpp
+++ main.cpp
@@ -3,16 +3,11 @@
 #include <sstream>
 #include <string>
 
-2513826 PrimFSM.cpp(37) FSM: Sg.SIP.UA id: 2621; > St: 0 NUL Pr: 38418:21 SIP_UA_OPTIONS_REQ ()
-2026-01-12 09:06:03.384
-  
-struct timestamp
-{
-    timestamp(const std::sstream& ss)
-    {
+// 2513826 PrimFSM.cpp(37) FSM: Sg.SIP.UA id: 2621; > St: 0 NUL Pr: 38418:21 SIP_UA_OPTIONS_REQ ()
+// 2026-01-12 09:06:03.384
 
-    }
-    
+struct timestamp
+{    
     struct Date
     {
         int day;
@@ -27,16 +22,48 @@ struct timestamp
         int ss;
         int ms;
     } time;
-}
 
-struct LogFormat
-{
-    std::string timestamp;
-}
+    timestamp(std::stringstream& ss)
+    {
+        ss >> *this;  
+    }
 
+    friend std::stringstream& operator>>(std::stringstream& ss, timestamp& ts)
+    {
+        // Untrached wrong time
+        char sep;
+        
+        ss >> ts.date.year >> sep >> ts.date.month >> sep >> ts.date.day
+           >> ts.time.hh >> sep >> ts.time.mm >> sep >> ts.time.ss >> sep >> ts.time.ms;
+        
+        return ss;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const timestamp& ts)
+    {
+        os << ts.date.year << "-" 
+           << (ts.date.month < 10 ? "0" : "") << ts.date.month << "-"
+           << (ts.date.day < 10 ? "0" : "") << ts.date.day << " "
+           << (ts.time.hh < 10 ? "0" : "") << ts.time.hh << ":"
+           << (ts.time.mm < 10 ? "0" : "") << ts.time.mm << ":"
+           << (ts.time.ss < 10 ? "0" : "") << ts.time.ss << "."
+           << (ts.time.ms < 100 ? (ts.time.ms < 10 ? "00" : "0") : "") << ts.time.ms;
+        
+        return os;
+    }
+};
 
 int main()
 {
-    // one stringstream for fill all structures
+    std::string raw = "2026-01-12 09:06:03.384 2026-02-11 19:06:03.084";
+    
+    // One stream for all parcing
+    std::stringstream ss(raw);
+    timestamp ts1(ss);
+    std::cout << "ts1: " << ts1 << std::endl;
+
+    timestamp ts2(ss);
+    std::cout << "ts2: " << ts2 << std::endl;
+
     return 0;
 }
